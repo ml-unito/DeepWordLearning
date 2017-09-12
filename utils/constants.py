f1c1cf6b4d6f882f0508563a37dc5ea1e912447e
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2017-09-12 11:45:26 +0200

    Add new constants.py file
    Former-commit-id: 7a2a073eb834af884f7d5bb98352f4379481edd4

---
+++ utils/constants.py
@@ -0,0 +1,13 @@
+import os, logging
+
+class Constants():
+    # this actually refers to the python working dir
+    # so you have to take care to run scripts from the
+    # root folder - which is DeepWordLearning/
+    ROOT_FOLDER = os.path.abspath('.')
+    DATA_FOLDER = os.path.join(ROOT_FOLDER, 'data')
+    AUDIO_DATA_FOLDER = os.path.join(DATA_FOLDER, 'audio')
+    AVAILABLE_SPEAKERS = ['tom', 'allison', 'daniel', 'ava', 'lee', 'susan', 'tom-130', 'allison-130', 'daniel-130', \
+                          'ava-130', 'lee-130', 'susan-130']
+    LOGGING_LEVEL = logging.INFO
+
