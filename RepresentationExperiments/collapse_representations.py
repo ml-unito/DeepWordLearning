12c9acb8fcd594f21b5e8e14a6aff5336edb64d6
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2018-02-06 19:06:41 +0100

    Add collapse_representation.py

---
+++ RepresentationExperiments/collapse_representations.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pickle 
+import data_utils
+
+def collapse_activations(xs, ys, thresh=0.00001):
+    new_activations = []
+    for x in xs:
+        old_shape = x.shape
+        x_shift = np.roll(x, -1)
+        x_temp = x - x_shift
+        x_temp = np.delete(x_temp, -1, axis=0) 
+
+        mask = np.greater_equal(np.mean(x_temp, axis=1), thresh)
+        avg_activation = 0
+        avg_i = 0
+        out = []
+        for i, xi in enumerate(x_temp):
+            avg_activation += xi
+            avg_i += 1
+            if mask[i] == True:
+                out.append(avg_activation / avg_i)
+                avg_acc = 0
+                avg_i = 0
+        out = np.array(out)
+        #print('Old shape: {} new shape: {}'.format(old_shape, out.shape))
+        new_activations.append(out)
+    np.array(new_activations)
+    return new_activations
+    
+
+
+if __name__ == '__main__':
+    xs, ys = data_utils.load_data('/home/cerrato/activations-allspeakers.pkl')
+    new = collapse_activations(xs, ys)
+    with open('activations-collapsed', 'wb') as f:
+        pickle.dump(new, f)
