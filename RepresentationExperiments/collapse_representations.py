8cae206163edeca60501b6d8e77f1d458577489a
Roberto Esposito <roberto.esposito@unito.it>
2018-02-07 11:43:05 +0100

    Cosmetic changes
    Clean up unused variables.
    Add a printout that clarifies how well it is performing.

--- RepresentationExperiments/collapse_representations.py
+++ RepresentationExperiments/collapse_representations.py
@@ -1,20 +1,19 @@
 import numpy as np
-import pickle 
+import pickle
 import data_utils
 
 def collapse_activations(xs, ys, thresh=30):
     new_activations = []
     for x in xs:
-        old_shape = x.shape
         x_shift = np.roll(x, -1)
         x_temp = x - x_shift
-        x_temp = np.delete(x_temp, -1, axis=0) 
+        x_temp = np.delete(x_temp, -1, axis=0)
 
         mask = np.greater_equal(np.linalg.norm(x_temp, ord=2, axis=1), thresh)
         avg_activation = np.zeros(x.shape[1])
         count = 0
         out = []
-        for i, xi in enumerate(x_temp):
+        for i,_ in enumerate(x_temp):
             avg_activation += x[i]
             count += 1
             if mask[i] == True:
@@ -22,15 +21,17 @@ def collapse_activations(xs, ys, thresh=30):
                 avg_activation = np.zeros(x.shape[1])
                 count = 0
         out = np.array(out)
-        #print('Old shape: {} new shape: {}'.format(old_shape, out.shape))
         new_activations.append(out)
+
+        print("len(x):%d len(new x):%d" % (len(x), len(out)))
+
     np.array(new_activations)
     return new_activations
-    
+
 
 
 if __name__ == '__main__':
-    xs, ys = data_utils.load_data('/home/cerrato/activations-allspeakers.pkl')
+    xs, ys = data_utils.load_data('../activations.pkl')
     new = collapse_activations(xs, ys)
-    with open('activations-collapsed', 'wb') as f:
+    with open('activations-collapsed.pkl', 'wb') as f:
         pickle.dump(new, f)
