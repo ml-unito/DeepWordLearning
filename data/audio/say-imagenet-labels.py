eeb62a28d6b43790e4c3b0f644ca319587d80cfb
Mattia Cerrato <matcerrato@gmail.com>
2019-01-18 11:23:09 +0100

    say-imagenet-labels.py: Update to support multiple speakers

--- data/audio/say-imagenet-labels.py
+++ data/audio/say-imagenet-labels.py
@@ -7,32 +7,33 @@ def tokenize_label_words(imagenet_words_string):
     return imagenet_words_string.split(',')
 
 if __name__ == '__main__':
+    speakers = ['tom', 'karen', 'lee', 'moira', 'tessa', 'daniel', 'kate', 'oliver', 'serena', 'kathy', 'samantha', 'vicki', 'victoria']
     args = sys.argv[1:]
     if len(args) > 3:
-        print('Usage: python3 say-imagenet-labels [imagenet-label-file] [dataset-output-path] [optional: speaker name]')
+        print('Usage: python3 say-imagenet-labels [imagenet-label-file] [dataset-output-path] [rate]')
         sys.exit(1)
+    if len(args) == 3:
+        rate = args[2]
+    else:
+        rate = 'default'
     with open(args[0]) as imagenet_labels_file:
         labels_dict = json.load(imagenet_labels_file)
 
     basepath = args[1]
-    if len(args)> 3:
-        speaker = args[3]
-    else:
-        speaker = 'tom'
-    print('Chosen speaker: '+ speaker)
 
-    for key, value in labels_dict.items():
-        if int(key) < 1000:
-            continue
-        # Create a new directory for the labels audio files, if needed
-        target_dir = os.path.join(basepath, str(key))
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-        word_list = tokenize_label_words(value)
-        i = 1000
-        for word in word_list:
-            if args[2] != 'default':
-                subprocess.run(['say', '-v', str(speaker), word, '-o', os.path.join(target_dir, str(i) + '.aiff'), '-r', str(args[2])])
-            else:
-                subprocess.run(['say', '-v', str(speaker), word, '-o', os.path.join(target_dir, str(i) + '.aiff')])
-            i += 1
+    for speaker in speakers:
+        print('Processing {}'.format(speaker))
+        speaker_dir = os.path.join(basepath, speaker)
+        os.makedirs(speaker_dir)
+        for key, value in labels_dict.items():
+            # Create a new directory for the labels audio files, if needed
+            target_dir = os.path.join(speaker_dir, str(key))
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
+            word_list = tokenize_label_words(value)
+            for i, word in enumerate(word_list):
+                if rate != 'default':
+                    subprocess.run(['say', '-v', str(speaker), word, '-o', os.path.join(target_dir, str(i) + '.aiff'), '-r', rate])
+                else:
+                    subprocess.run(['say', '-v', str(speaker), word, '-o', os.path.join(target_dir, str(i) + '.aiff')])
+                i += 1
